3b98a566bcbcba31dfe939ecb3da4452780242ef
gc <guocai1985@163.com>
2025-11-08 23:14:28 +0800

    添加提示词相关的文本内容

--- model/model.py
+++ model/model.py
@@ -1,6 +1,6 @@
 from typing import Set
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, SmallInteger, Text
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
@@ -17,6 +17,21 @@ class BaseModel(Base):
 class Prompt(BaseModel):
     """测试用例"""
     __tablename__ = "t_prompt"
-    id = Column(Integer, primary_key=True)
-    category = Column(String, nullable=False)
-    question = Column(String, nullable=False)
+    id = Column(Integer, primary_key=True, doc="主键")
+    prompt_id = Column(String, length=36, unique=True, nullable=False, doc="用于关联其它表，唯一")
+    type = Column(String, nullable=False, doc="类型")
+    level = Column(SmallInteger, default=1, doc="级别")
+    prompt = Column(String, length=512, nullable=False, doc="提示词内容")
+    response = Column(String, length=1024, nullable=True, doc="参考回答")
+
+
+class Record(BaseModel):
+    """测试记录"""
+    __tablename__ = "t_record"
+    id = Column(Integer, primary_key=True, doc="主键")
+    record_id = Column(String, length=36, unique=True, nullable=False, doc="用于关联其它表，唯一")
+    type = Column(String, nullable=False, doc="类型")
+    level = Column(SmallInteger, default=1, doc="级别")
+    prompt = Column(String, length=512, nullable=False, doc="提示词内容")
+    response = Column(Text, nullable=True, doc="大模型应答内容")
+    score = Column(SmallInteger, default=1, doc="评测得分")
